7e3579e29d6962f357e863a93e959e89f1b000d3
Martha Romero <myromero14@gmail.com>
2023-02-14 17:04:33 -0800

    completed assignment

--- js/script.js
+++ js/script.js
@@ -1,3 +1,179 @@
 // STEP 1
+/* 
+    let someMonth;
+    function theMonth();
+    let currentMonth;
+    let summerMonth;
+    let MyLibraryFunction;
+*/
+
+
 // STEP 2
+/* 
+    123456              numerical literal expression
+    'hello world'       string literal 
+    false               Boolean literal
+    null                null literal
+*/
+
 // STEP 3
+/* 
+    let exampleOne = 38 * (24 + 17);
+    let exampleTwo = (210/8) + 77 - 3;
+*/
+
+// STEP 4
+/*
+        let strFirstName;
+        let strLastName;
+        let strAddress;
+        let strCity;
+        let strState;
+        let intZipCode;
+        let intAge;
+        let strReferralSource;
+        let blnMayWeContact;
+*/
+
+//STEP 5
+//Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables.
+/*
+    let strFirstName;
+    strFirstName = 'Barry';
+
+    let strLastName = 'Allen';
+
+    let age = 25, city = 'Central City';
+*/
+
+//STEP 6
+//Create a variable.
+//Add a number and a string and display the coerced result in the browser’s console window.
+/*
+    let firstName = 'Lois';
+    let yearsWorking = 15;
+    console.log( firstName + ' has been a reporter for ' + yearsWorking + ' years.');
+*/
+
+//STEP 7
+//Create two variables.
+//For the first variable, add a Boolean and a string and display the coerced result.
+//For the second variable, add a number and a Boolean and display the coerced result.
+/*
+    let firstName = 'Dinah';
+    let city = 'Seattle';
+    let x = true;
+    let totalYears = 5 + x;
+
+    console.log(firstName + ' currently lives in ' + city + " : " + x);
+
+    console.log(firstName + ' has lived in ' + city + " for " + totalYears + " years");
+*/
+
+//STEP 8
+/* 
+No, because the apostrophe in I'm causes an error. To fix it, you would add a backslash (\) escape character
+
+    let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+    console.log(someString)
+*/
+
+//STEP 9
+/*
+    let x = null;
+    console.log(x);
+
+    let y;
+    console.log(y);
+*/
+
+//STEP 10
+/*
+    let x;
+    let turtle = new Object();
+    console.log(typeof 'hello world', typeof 21, typeof false, typeof turtle, typeof x);
+*/
+
+//STEP 11
+/*
+    alert('Hello ' + 'Martha Romero' + ', welcome to the JavaScript class!');
+*/
+
+//STEP 12
+/*
+    let student= 'Martha Romero';
+    alert('Hello ' + student + ', welcome to the JavaScript class!');
+*/
+
+//STEP 13
+/*
+    let student= 'Martha Romero';
+    let course = 'JavaScript'
+    alert('Hello ' + student + ', welcome to the ' + course + ' class!');
+*/
+
+//STEP 14
+/*
+    let student= 'Martha Romero';
+    let course = 'JavaScript'
+    alert('Hello ' + student + '.' + '\nWelcome to the ' + course + ' class!');
+*/
+
+//STEP 15    
+/*
+    let student= prompt('Please enter your name: ');
+    let course = 'JavaScript'
+    alert('Hello ' + student + '.' + '\nWelcome to the ' + course + ' class!');
+*/
+
+//STEP 16
+/*
+    let student= prompt('Please enter your name: ');
+    let course = prompt('What class are you taking?')
+    alert('Hello ' + student + '.' + '\nWelcome to the ' + course + ' class!');
+*/
+
+//STEP 17
+/*
+    let x = 10;
+    let y = 20;
+    console.log(x+y);
+*/
+
+//STEP 18
+/*
+    let x = 20
+    x += 20
+    console.log(x)
+*/
+
+//STEP 19
+/*
+    let x = 20
+    x *= 5
+    console.log(x)
+*/
+
+//STEP 20
+/*
+    let x = 20%3;
+    x /= 1;
+    console.log(x);
+*/
+
+//STEP 21
+/*
+    let x = 50;
+    let y = 100;
+    console.log(x == 50 && y == 100);
+*/
+
+//STEP 22
+/*
+    let x = 50;
+    let y = 100;
+    console.log(x != 50 || y != 100);
+*/
+
+
+
